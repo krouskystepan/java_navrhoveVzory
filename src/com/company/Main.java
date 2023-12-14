94343bc6a31d1a4b79eb839ab5db878972c36305
Stepan Krousky <stepan.krousky@seznam.cz>
2023-12-14 11:18:04 +0100

    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+        Singleton x = Singleton.getInstance();
+        Singleton y = Singleton.getInstance();
+
+        MobileFactory factory = new MobileFactory();
+        OS phone = factory.getPhone("Samsung");
+        phone.type();
+
         PCBuilder builer = new PCBuilder();
         PC pc = builer
                 .setColor("pink")
